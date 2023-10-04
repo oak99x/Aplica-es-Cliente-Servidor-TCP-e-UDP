dc1f6207032e7ce610aa59fbcc96dc9ab52525bd
Mateus <mateus.oak99@gmail.com>
2023-10-04 17:44:31 -0300

    registro de nome - bug concorrencia/threads

--- teste2/client.py
+++ teste2/client.py
@@ -4,43 +4,54 @@ import threading
 def receber_mensagens(client_socket):
     while True:
         mensagem = client_socket.recv(1024).decode('utf-8')
+        if not mensagem:
+            # Quando o encerramento da aplicação é solicitado, feche as conexões
+            client_socket.close()
+            break
         print(mensagem)
 
-def enviar_mensagem(client_socket):
+def enviar_mensagem(client_socket_data, client_socket_control):
     while True:
-        mensagem = input()
-        client_socket.send(mensagem.encode('utf-8'))
-    
+        try:
+            mensagem = input()
+            client_socket_data.send(mensagem.encode('utf-8'))
+            client_socket_control.send(mensagem.encode('utf-8'))
+        except:
+            break
+        
+def registrar_nome(client_socket_control):
+        nome = input("Digite seu nome: ")
+        client_socket_control.send(nome.encode('utf-8'))
+
 def main():
     host = 'localhost'  # Endereço do servidor
     port_data = 8080    # Porta para mensagens de chat
     port_control = 8081 # Porta para mensagens de controle
-
-    # Registre a entrada no servidor
-    nome = input("Digite seu nome: ")
 
     client_socket_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket_data.connect((host, port_data))
 
     client_socket_control = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket_control.connect((host, port_control))
-    client_socket_control.send(nome.encode('utf-8'))
+
+    # Registre a entrada no servidor
+    #bug com as threads
+    #não ta sendo executado, pois está sendo atropelado pela concorrencia e pula direto para a função enviar_mensagem
+    #registrar_nome(client_socket_control)
+    print("Digite seu nome: ")
 
     # Inicie threads para receber e enviar mensagens
     thread_receber_control = threading.Thread(target=receber_mensagens, args=(client_socket_control,))
-    thread_enviar_control = threading.Thread(target=enviar_mensagem, args=(client_socket_control,))
-
     thread_receber = threading.Thread(target=receber_mensagens, args=(client_socket_data,))
-    thread_enviar = threading.Thread(target=enviar_mensagem, args=(client_socket_data,))
     
     thread_receber_control.start()
-    thread_enviar_control.start()
-
     thread_receber.start()
-    thread_enviar.start()
 
-    while True:
-        pass
+    enviar_mensagem(client_socket_data, client_socket_control)
+    
+    # Quando o envio de mensagens é encerrado, espere que as threads de recebimento terminem
+    thread_receber_control.join()
+    thread_receber.join()
 
 if __name__ == "__main__":
     main()
--- teste2/server.py
+++ teste2/server.py
@@ -16,11 +16,13 @@ def broadcast(mensagem, cliente):
 
 def remover(nome, cliente):
     if nome in clientes:
+        print(nome + " desconectou-se do servidor.") #print no servidor
+        clientes[nome][0].send("Voce desconectou-se do chat.".encode('utf-8')) #print para o cliente
+        broadcast(f"{nome} desconectou-se do chat.".encode('utf-8'), cliente) #print para todos os clientes
+
         clientes[nome][0].close()
         clientes[nome][1].close()
         del clientes[nome]
-        print(nome + " desconectou-se do servidor.")
-        broadcast(f"{nome} desconectou-se do chat.".encode('utf-8'), cliente)
         
             
 
@@ -31,7 +33,7 @@ def lidar_com_cliente(cliente):
     if nome not in clientes:
         clientes[nome] = [conexao_data, conexao_control]
         print(nome + " conectou-se ao servidor.") #print no servidor
-        conexao_data.send("Voce conectou-se ao servidor.".encode('utf-8')) #print para o cliente
+        conexao_data.send("Voce conectou-se ao chat.".encode('utf-8')) #print para o cliente
         broadcast(f"{nome} conectou-se ao chat.".encode('utf-8'), conexao_data) #print para todos os clientes
     else:
         clientes[nome].append(conexao_data)
@@ -40,36 +42,29 @@ def lidar_com_cliente(cliente):
     while True:
         try:
             mensagem_data = conexao_data.recv(1024).decode('utf-8')
-            mensagem_control = conexao_control.recv(1024).decode('utf-8') #arrumar cliente para que fique enviando algo em branco
+            mensagem_control = conexao_control.recv(1024).decode('utf-8')
 
-            if mensagem_data:
-                if mensagem_data.startswith('/MSG'):
-                    mensagem_broadcast = f"[{nome}]: {mensagem_data[5:]}"
-                    broadcast(mensagem_broadcast.encode('utf-8'), conexao_data)
+            if mensagem_data.startswith('/MSG'):
+                mensagem_broadcast = f"[{nome}]: {mensagem_data[5:]}"
+                broadcast(mensagem_broadcast.encode('utf-8'), conexao_data)
 
-                elif mensagem_data.startswith('/PRIV'):
-                    partes = mensagem_data.split(' ')
-                    destinatario = partes[1]
-                    mensagem_privada = ' '.join(partes[2:])
+            elif mensagem_data.startswith('/PRIV'):
+                partes = mensagem_data.split(' ')
+                destinatario = partes[1]
+                mensagem_privada = ' '.join(partes[2:])
 
-                    if destinatario in clientes:
-                        conexao_destinatario = clientes[destinatario][0]
-                        mensagem_privada = f"[PRIVADO de {nome}]: {mensagem_privada}".encode('utf-8')
-                        conexao_destinatario.send(mensagem_privada)
-                    else:
-                        conexao_data.send("Usuário não encontrado.".encode('utf-8'))
-                elif mensagem_data.startswith('/EXIT'):
-                    remover(nome, conexao_data)
-                    break  # Sair do loop quando o cliente se desconectar
+                if destinatario in clientes:
+                    conexao_destinatario = clientes[destinatario][0]
+                    mensagem_privada = f"[PRIVADO de {nome}]: {mensagem_privada}".encode('utf-8')
+                    conexao_destinatario.send(mensagem_privada)
                 else:
-                    conexao_data.send("Comando inválido.".encode('utf-8'))
+                    conexao_data.send("Usuário não encontrado.".encode('utf-8'))
             
-            elif mensagem_control:
-                if mensagem_control.startswith('/EXIT'):
-                    remover(nome, conexao_control)
-                    break  # Sair do loop quando o cliente se desconectar
-                else:
-                    conexao_data.send("Comando de controle inválido.".encode('utf-8'))
+            elif mensagem_control.startswith('/EXIT'):
+                remover(nome, conexao_control)
+                break  # Sair do loop quando o cliente se desconectar
+            else:
+                conexao_data.send("Comando inválido.".encode('utf-8'))
         except:
             remover(nome, conexao_data)
             break
