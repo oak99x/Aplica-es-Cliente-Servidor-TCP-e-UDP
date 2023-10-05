c4f2e63a11c1decf492391919ba027f4b6dab954
Gustavo <50451202+gustavord@users.noreply.github.com>
2023-10-05 17:48:29 -0300

    funcional

--- UDPserver.py
+++ UDPserver.py
@@ -1,69 +1,72 @@
 import socket
+import threading
 
 clientes = {}
 
 def broadcast_to_others(mensagem, cliente_enviador):
-    for cliente, conexao in clientes.items():
+    for cliente in clientes:
         if cliente != cliente_enviador:
             try:
-                conexao.sendto(mensagem, ('255.255.255.255', 8080))
+                server_socket.sendto(mensagem, cliente)
             except:
-                remover(cliente_enviador, conexao)
+                print("erro")
+                #remover(cliente, conexao)
 
-def lidar_com_cliente(cliente):
-    nome, conexao = cliente
-    clientes[nome] = conexao
-    print(nome + " conectou-se ao servidor.")
-    conexao.sendto(f"{nome} conectou-se do servidor.".encode('utf-8'), ('255.255.255.255', 8080))
-    
-    while True:
-        try:
-            mensagem, sender = conexao.recvfrom(1024)
-            if mensagem:
-                mensagem = mensagem.decode('utf-8')
-                if mensagem.startswith('/PRIVMSG'):
-                    partes = mensagem.split(' ')
-                    destinatario = partes[1]
-                    mensagem_privada = ' '.join(partes[2:]) ## PROCURAR COM DICT
-                    if destinatario in clientes:
-                        dest = clientes[destinatario][0]
-                        dest.sendto(f"[PRIVADO de {nome}]: {mensagem_privada}".encode('utf-8'), (dest[1]))
-                    else:
-                        conexao.sendto("Usuário não encontrado.".encode('utf-8'), sender)
-                elif mensagem.startswith('/MSG'):
-                    mensagem_broadcast = f"[{sender}]: {mensagem[5:]}"
-                    broadcast_to_others(mensagem_broadcast.encode('utf-8'), sender)
-                elif mensagem.startswith('/EXIT'):
-                    remover(sender, conexao)
+def lidar_com_cliente(cliente_info, nome):
+    data, addr = cliente_info
+    mensagem = data.decode('utf-8')
+
+    try:
+        if mensagem:
+            if mensagem.startswith('/PRIVMSG'):
+                partes = mensagem.split(' ')
+                destinatario = partes[1]
+                mensagem_privada = ' '.join(partes[2:])
+
+                for cliente in clientes.items():
+                    if cliente[1] == destinatario:
+                        dest = cliente[0]
+                        server_socket.sendto(f"[PRIVADO para {destinatario}]: {mensagem_privada}".encode('utf-8'), dest)
+                        break
                 else:
-                    conexao.send("Comando inválido.".encode('utf-8'))
-        except:
-            remover(sender, conexao)
-            break
+                    server_socket.sendto("Usuário não encontrado.".encode('utf-8'), addr)
+            elif mensagem.startswith('/MSG'):
+                mensagem_broadcast = f"[{nome}]: {mensagem[5:]}"
+                broadcast_to_others(mensagem_broadcast.encode('utf-8'), addr)
+            elif mensagem.startswith('/EXIT'):
+                remover(nome, addr)
+            else:
+                server_socket.sendto("Comando inválido.".encode('utf-8'), addr)
+    except:
+        remover(nome, addr)
 
 def remover(nome, conexao):
-    if nome in clientes:
-        print(nome[0] + " desconectou-se do servidor.")
-        del clientes[nome]
-        conexao.sendto("{nome} desconectou-se do servidor.".encode('utf-8'), ('255.255.255.255', 8080))
-        conexao.close()
+    if nome in clientes.values():
+        server_socket.sendto("--X--".encode('utf-8'), conexao)
+        del clientes[conexao]
+        broadcast_to_others(f"{nome} desconectou-se do servidor".encode('utf-8'), ('0', 8080))
 
 def main():
-    # create a socket object
+    global server_socket
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     host = '0.0.0.0'
     port = 8080
-    # bind to the port
     server_socket.bind((host, port))
-    print("Server started")
+    print("--- Server online ---")
 
     while True:
-        # receive data from the client
         data, addr = server_socket.recvfrom(1024)
-        print("received message: %s" % data.decode('utf-8'))
+        cliente_info = (data, addr)
 
-        # send data to the client
-        server_socket.sendto(data, addr)
+        if addr not in clientes:
+            nick = data.decode('utf-8')
+            clientes[addr] = nick
+            print(f"Usuario {nick} logado.")
+            broadcast_to_others(f"{nick} entrou na sala.".encode('utf-8'), addr)
+        else:
+            nome = clientes[addr]
+            lidar_com_cliente(cliente_info, nome)
 
 if __name__ == '__main__':
     main()
